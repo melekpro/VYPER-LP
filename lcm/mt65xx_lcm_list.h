a7ce64a4453395e22627cb8df49e30dc186dc125
melekpro <melekpro6@gmail.com>
2024-01-21 19:27:26 +0100

    Add files via upload

---
+++ lcm/mt65xx_lcm_list.h
@@ -0,0 +1,10 @@
+#ifndef __MT65XX_LCM_LIST_H__
+#define __MT65XX_LCM_LIST_H__
+
+#include <lcm_drv.h>
+extern LCM_DRIVER st7701_fwgaplus_dsi_vdo_helitai_vp531_lcm_drv;
+#ifdef BUILD_LK
+extern void mdelay(unsigned long msec);
+#endif
+
+#endif
